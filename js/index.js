78de05b1a0129ab8015535a8360b7b8346b3199f
Kevin Hubbard <khubbard@uda1.com>
2018-02-07 15:08:18 -0600

    adding tabletop.js and working on header animations

--- js/index.js
+++ js/index.js
@@ -11,8 +11,6 @@ $(document).ready(function() {
 
       ::FIX::
       -font stuff
-
-      test commit
   */
 });
 
@@ -20,8 +18,33 @@ function setupPage() {
   //binds all necessary things and click events, etc
   $(".parallax").parallax();
 
-  $("#site_navigation span").on("click", function() {
-    $("#site_navigation span").removeClass("nav_active blue lighten-3");
-    $(this).addClass("nav_active blue lighten-3");
+  $("#site_navigation .nav_button").on("click", function() {
+    $("#site_navigation .nav_button").removeClass("active blue lighten-3");
+    $(this).addClass("active blue lighten-3").removeClass("grey lighten-2");
+  }).on("mouseover", function() {
+    if(!($(this).hasClass("active")))
+      $(this).addClass("grey lighten-2");
+  }).on("mouseleave", function() {
+    $(this).removeClass("grey lighten-2");
   });
-}
+
+  animation.headerTitleMinimize();
+};
+
+var animation = {
+  /*headerTitleMinimize: function() {
+    Materialize.scrollFire([
+      {selector: "#site_navigation", offset: window.innerHeight - 200, callback: function() {
+          $("#site_header #cah_title_area .main-title").toggleClass("minimized");
+          $("#site_header #cah_title_area").toggleClass("minimized");
+        }
+      }
+    ]);
+  }*/
+};
+
+
+
+
+//https://medium.com/@jaejohns/how-to-use-google-sheets-as-your-website-database-b0f2f13d0396
+//https://stackoverflow.com/questions/13699589/display-the-content-of-a-google-sheet-cell-in-a-web-page
