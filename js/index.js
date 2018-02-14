a17787f841cad18b8ba4be55d9083456905bee59
khubbard96 <kevlar2020@gmail.com>
2018-02-14 00:05:54 -0600

    starting version 4 of cah website

--- js/index.js
+++ js/index.js
@@ -1,35 +1,21 @@
 $(document).ready(function() {
   /* Reset the page*/
-  $("body").scrollTop(0);
+  setupPage();
 
-  $('.nav_page_links').on('click', function() {
-    $('.nav_page_links').removeClass('active');
-    $('.nav_page_links a').removeClass('bold-text');
-    $(this).addClass('active');
-    $(this).find('a').addClass('bold-text');
+  $("#nav-mobile li a").on("click", function() {
+    Site.currentPage = $(this).data("nav");
+    $("#nav-mobile li").removeClass("active");
+    $(this).parent().addClass("active")
   });
-  $('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrainWidth: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'left', // Displays dropdown with edge aligned to the left of button
-      stopPropagation: false // Stops event propagation
-    }
-  );
-  $('.dropdown-content a').on('click',function() {
-    var goToLocation = $(this).data('link');
-    var headingLocation = $('body').find('#' + goToLocation).offset().top - 10; //top padding
-    $('body').animate({
-      scrollTop: headingLocation
-    }, 400, function() {
 
-    });
-  });
+
 });
 
 function setupPage() {
+  $(document).find("#nav-mobile li").removeClass("active");
+  $(document).find("#nav-mobile li [data-nav=" + Site.currentPage + "]").parent().addClass("active");
+}
 
+var Site = {
+  currentPage: "home"
 }
